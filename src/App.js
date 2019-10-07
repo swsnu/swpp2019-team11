c7e34cc6069ae6af5a319a4f43a11f82f260c86d
jangsus1 <46769685+jangsus1@users.noreply.github.com>
2019-10-07 16:46:34 +0900

    Revert "Login form designed with semenctic-ui-react"

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
 import React from 'react';
-import LoginPage from './containers/LoginPage/LoginPage'
-import SignupPage from './containers/SignupPage/SignupPage'
-import {BrowserRouter, Route, Redirect, Switch} from 'react-router-dom'
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Switch>
-          <Route path='/login' component = {LoginPage} />
-          <Route path = '/signup' component = {SignupPage}/>
-          <Redirect exact from='/' to = '/login'/>
-        </Switch>
-      </div>
-    </BrowserRouter>
+    <div className="App">
+
+    </div>
   );
 }
 
