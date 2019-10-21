452c707d233c9ef83db1af0b9ea217184ada2ca6
jangsus1 <min99101511@naver.com>
2019-10-22 00:45:10 +0900

    setting gitignore and reducers for redux

---
+++ front/src/store/store.js
@@ -0,0 +1,29 @@
+import {createStore, combineReducers, applyMiddleware} from 'redux'
+import thunk from 'redux-thunk'
+
+import SurveyListReducer from './reducers/SurveyListReducer'
+import SurveyReducer from './reducers/SurveyReducer'
+import UserReducer from './reducers/UserReducer'
+import CartReducer from './reducers/CartReducer'
+
+const rootReducer = combineReducers({
+  us : UserReducer,
+  sv : SurveyReducer,
+  svl : SurveyListReducer,
+  ct : CartReducer,
+})
+
+const logger = store => {
+  return next => {
+    return action => {
+      console.log('[Middleware] Dispatching', action)
+      const result = next(action)
+      console.log('[Middleware] Next State', store.getState())
+      return result
+    }
+  }
+}
+
+const store = createStore(rootReducer, applyMiddleware(logger, thunk))
+
+export default store
