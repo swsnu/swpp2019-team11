b149a26d51fb2cb4be719a76f15d51a42f5d7d6e
jangsus1 <min99101511@naver.com>
2019-10-24 20:02:35 +0900

    added default case and installed linters

--- front/src/index.js
+++ front/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
+import 'semantic-ui-css/semantic.min.css';
+import store from './store/store';
 import App from './App';
-import 'semantic-ui-css/semantic.min.css'
-import store from './store/store'
-import {Provider} from 'react-redux'
 
-ReactDOM.render(<Provider store = {store}><App /></Provider>, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
