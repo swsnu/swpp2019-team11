fb793ebf49f1ca50c4826aaf4a6b7b0020dd6fab
geeglegeegle45 <minjung0-0@snu.ac.kr>
2019-10-30 22:02:16 +0900

    actions/signup & actions/index

---
+++ frontend/src/store/actions/signup.js
@@ -0,0 +1,19 @@
+import * as actionTypes from './actionTypes';
+import axios from 'axios';
+export const signup_ = (res) => {
+    //if (res.status == 200) alert("signed up"); modify this. 
+    return {type: actionTypes.SIGNUP};
+};
+export const signup = (username, email, password) => {
+    var user = {
+        username : username,
+        email : email,
+        password : password
+    }
+    return dispatch => {
+        return axios.post('/api/login/', user)
+            .then(res => {
+                    dispatch(signup_(res.data));
+            })
+    }
+};
