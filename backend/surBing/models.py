65b1a6131ce09cb87c990bace57e5441560f2fd2
jangsus1 <min99101511@naver.com>
2019-10-30 15:15:23 +0900

    some of reducer implemented

--- backend/surBing/models.py
+++ backend/surBing/models.py
@@ -4,23 +4,22 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Survey(models.Model):
     title = models.CharField(max_length=120)
-    author = models.ForeignKey(User, on_delete = models.CASCADE related_name = author)
+    author = models.ForeignKey(User, on_delete = models.CASCADE, related_name = 'author')
     date = models.TextField()
     content = models.TextField()
     response_count = models.IntegerField()
-    item = models.ManyToManyField(Item)
+    item = models.ManyToManyField('Item')
 
 class Item(models.Model):
     title = models.CharField(max_length=120)
     response_count = models.IntegerField()
     question_type = models.CharField(max_length=10)
     most_three_response = models.BooleanField()
-    response = models.ManyToManyField(Response)
+    response = models.ManyToManyField('Response')
 
 class Response(models.Model):
-    item = models.ForeignKey(Item, on_delete = models.CASCADE, related_name=item)
     content = models.TextField()
 
 class Cart(models.Model):
-    user - models.ForeignKey(User, on_delete = CASCADE)
-    survey = models.ManyToManyField(Survey)
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    survey = models.ManyToManyField('Survey')
---
+++ front/src/store/actions/index.js
@@ -0,0 +1,1 @@
+export {getSurveys} from './surveys'
