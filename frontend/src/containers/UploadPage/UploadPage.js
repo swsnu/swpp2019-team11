6ec4b648ecf0554e845aeb5807ffce85659b97a4
geeglegeegle45 <minjung0-0@snu.ac.kr>
2019-10-31 04:39:16 +0900

    apply Topbar

--- frontend/src/containers/UploadPage/UploadPage.js
+++ frontend/src/containers/UploadPage/UploadPage.js
@@ -4,19 +4,13 @@ import Submit from '../../components/UploadPage/Submit/Submit';
 import ProfileButton from '../../components/ProfileButton/ProfileButton';
 import React, { Component } from 'react';
 import { Grid, Header, Segment } from 'semantic-ui-react';
-
+import TopBar from '../../components/TopBar/TopBar';
 class UploadPage extends Component{
     render(){
         return (
         <div>
             <Segment style={{ minHeight: '10vh' }}>
-                <Segment 
-                    textAlign="left" style={{ minWidth: 200 }} width={2}>
-                    <Header style={{ 'font-size': '4em' }} size="huge" color="teal" > 
-                        surBing 
-                        <Segment style={{ minWidth: 200 }} floated="right" width={2}><ProfileButton /></Segment>
-                    </Header>
-                </Segment>
+                <TopBar/>
                 <UploadFile/>
                 <EditItem/>
                 <Submit/>
