bebb4466ca368c3d51a0e98667897a5ccae23fe1
kdh9949 <kdh9949@snu.ac.kr>
2019-11-01 19:14:42 +0900

    make SurBingUser

--- backend/surBing/models.py
+++ backend/surBing/models.py
@@ -1,13 +1,10 @@
 from django.db import models
-from django.contrib.auth.models import User
+from users.models import SurBingUser
 
 # Create your models here.
-class SurBingUser(User):
-    cart = models.ForeignKey(Cart, related_name='cart')
-
 class Survey(models.Model):
     title = models.CharField(max_length=120)
-    author = models.ForeignKey(User, on_delete = models.CASCADE, related_name = 'author')
+    author = models.ForeignKey(SurBingUser, on_delete=models.CASCADE, related_name='author')
     date = models.TextField()
     content = models.TextField()
     response_count = models.IntegerField()
@@ -24,5 +21,4 @@ class Response(models.Model):
     content = models.TextField()
 
 class Cart(models.Model):
-    user = models.ForeignKey(User, on_delete = models.CASCADE)
     survey = models.ManyToManyField('Survey')
