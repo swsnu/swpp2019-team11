bda9fcd73fff79c9004eeef08b7f136bf5b387d3
Kojihyung <kokoleerh@gmail.com>
2019-11-02 08:11:53 +0900

    link backend

--- frontend/src/components/SurveyDetailPage/SurveyItem/ItemResponse/ItemResponse.js
+++ frontend/src/components/SurveyDetailPage/SurveyItem/ItemResponse/ItemResponse.js
@@ -7,10 +7,10 @@ const ItemResponse = (props) => {
     <div className = "ItemResponse">
       <Message.List color = '#F8FFFF' style={{'min-width' : '700px',border:'1.5px solid #CFCFCF', background: '#e1fff5', borderRadius:5, 'font-size':'0.8em', color: '#484749'}}>
         <Message.Item style = {{'margin-top': 10, 'margin-left' :30}}>
-          {props.itemResponse}
+          {props.id}
         </Message.Item>
         <Message.Item style = {{'margin-bottom': 10, 'margin-left' :30}}>
-          {props.itemResponse}
+          {props.id}
         </Message.Item>
       </Message.List>
 
--- frontend/src/containers/SurveyDetailPage/SurveyDetailPage.js
+++ frontend/src/containers/SurveyDetailPage/SurveyDetailPage.js
@@ -2,9 +2,35 @@ import React, { Component } from 'react';
 import SurveyItem from '../../components/SurveyDetailPage/SurveyItem/SurveyItem';
 import { Header, Icon, Grid } from 'semantic-ui-react';
 import TopBar from '../../components/TopBar/TopBar';
+import {connect} from 'react-redux';
+import * as actionCreators from '../../store/actions/index';
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onSurveyDetail : id =>
+      dispatch(actionCreators.getSurvey(id))
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    title : state.sv.title
+  }
+}
 
 class SurveyDetailPage extends Component {
+  state = {
+    title : '',
+  };
+
+  componentDidMount(){
+    this.props.onSurveyDetail(this.props.match.params.id)
+    this.state.title = this.props.title;
+  };
+
   render() {
+    console.log(this.props.title);
+    this.state.title = this.props.title;
     return (
       <div className = "SurveyDetailPage">
         <TopBar searchBar = {true}/>
@@ -12,7 +38,7 @@ class SurveyDetailPage extends Component {
           <Grid.Row verticalAlign='middle' style = {{margin: 30, height: 130, border: '1px solid grey', 'box-shadow': '5px 3px 3px #BDBDBD', borderRadius: 10}}>
             <Grid.Column style = {{width: '65%', 'font-size':'3.5em'}}>
               <Header style = {{color: '#00B5AD'}} textAlign='center' vertical-align='middle'>
-                Survey Title
+                {this.state.title}
               </Header>
             </Grid.Column>
             <Grid.Column textAlign='center' style = {{width: '35%', 'font-size':'4em'}}>
@@ -21,11 +47,12 @@ class SurveyDetailPage extends Component {
             </Grid.Column>
           </Grid.Row>
         </Grid>
-
-        <SurveyItem />
+        <SurveyItem
+          id = {this.props.match.params.id}
+        />
       </div>
     )
   }
 }
 
-export default SurveyDetailPage;
+export default connect(mapStateToProps,mapDispatchToProps)(SurveyDetailPage);
