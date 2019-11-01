42108ad14aa967d88437ce419c2a79873900263a
kdh9949 <kdh9949@snu.ac.kr>
2019-11-01 09:32:56 +0900

    implement mycart view

--- backend/surBing/models.py
+++ backend/surBing/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class SurBingUser(User):
+    cart = models.ForeignKey(Cart, related_name='cart')
+
 class Survey(models.Model):
     title = models.CharField(max_length=120)
     author = models.ForeignKey(User, on_delete = models.CASCADE, related_name = 'author')
