758bca1de9148b631615833cddf579cdc5f7028c
jangsus1 <min99101511@naver.com>
2019-11-01 18:53:48 +0900

    test file implemented

--- frontend/src/components/ProfileButton/ProfileButton.test.js
+++ frontend/src/components/ProfileButton/ProfileButton.test.js
@@ -1,17 +1,21 @@
 import React from 'react'
 import {shallow, mount} from 'enzyme'
-import ProfileButton from './ProfileButton'
-import {MemoryRouter} from 'react-router-dom'
+import {ProfileButton} from './ProfileButton'
 
 describe('<ProfileButton />', ()=> {
   beforeEach(()=> {jest.clearAllMocks()})
   const props = {
-
+    history : {push : jest.fn()}
   }
-  const component = shallow(<MemoryRouter><ProfileButton {...props} /></MemoryRouter>)
+  const component = mount(<ProfileButton {...props} />)
   const instance = component.instance()
   it('should render without errors', () => {
-    const wrapper = component.find('.Dropdown')
-    expect(wrapper.length).toBe(1)
+    const wrapper = component.find('.DropDownClass')
+    expect(wrapper.length).toBe(3)
+  })
+  it('button should work', () => {
+    const wrapper = component.find('.DropDownClass')[1]
+    wrapper.simulate('click')
+    expect(props.history.push).toHaveBeenCalledTimes(1);
   })
 })
