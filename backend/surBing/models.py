7432af18d2f6309a92650030218f8d5eed54fe68
jangsus1 <min99101511@naver.com>
2019-11-04 02:37:53 +0900

    models migrated

--- backend/surBing/models.py
+++ backend/surBing/models.py
@@ -5,9 +5,9 @@ from users.models import SurBingUser
 class Survey(models.Model):
     title = models.CharField(max_length=120)
     author = models.ForeignKey(SurBingUser, on_delete=models.CASCADE, related_name='author')
-    upload_date = models.CharField(max_length=10)
-    survey_start_date = models.CharField(max_length=10)
-    survey_end_date = models.CharField(max_length=10)
+    upload_date = models.CharField(max_length=10, null=True)
+    survey_start_date = models.CharField(max_length=10, null=True)
+    survey_end_date = models.CharField(max_length=10, null=True)
     content = models.TextField()
     respondant_count = models.IntegerField()
     item = models.ManyToManyField('Item')
@@ -18,7 +18,7 @@ class Item(models.Model):
     response = models.ManyToManyField('Response')
 
 class Response(models.Model):
-    respondant_id = models.IntegerField()
+    respondant_id = models.IntegerField(null=True)
     content = models.TextField()
 
 class Cart(models.Model):
