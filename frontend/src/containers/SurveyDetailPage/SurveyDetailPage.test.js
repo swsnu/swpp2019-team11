0d168876084df9a4ad37801bd4a0ee4e76f946c1
jangsus1 <min99101511@naver.com>
2019-11-14 21:36:50 +0900

    working

--- frontend/src/containers/SurveyDetailPage/SurveyDetailPage.test.js
+++ frontend/src/containers/SurveyDetailPage/SurveyDetailPage.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow, mount } from 'enzyme';
 import {mapStateToProps, mapDispatchToProps, SurveyDetailPage} from './SurveyDetailPage'
 
-describe('SearchResultPage Shallow', () => {
+describe('SearchDetailPage Shallow', () => {
   beforeEach(() => { jest.clearAllMocks(); });
   const mockPush = jest.fn();
   const mockOnSurveyDetail = jest.fn()
@@ -15,13 +15,18 @@ describe('SearchResultPage Shallow', () => {
       id : 1,
       respondant_count : 10,
       survey_start_date : '1999-10-15',
-      survey_end_date : '2018-12-12'
+      survey_end_date : '2018-12-12',
+      item : [],
     }
   };
-  const component = shallow(<SurveyDetailPage {...props} />);
-  const instance = component.instance();
   it("should render without error", () => {
+    const component = shallow(<SurveyDetailPage {...props} />);
+    const instance = component.instance();
     const wrapper = component.find('.surveyDetailPage');
     expect(wrapper.length).toBe(1);
   })
+  it("")
 })
+
+
+describe("")
