fbf8339e820e71d4c1ce54baa95885209b4b08c5
geeglegeegle45 <minjung0-0@snu.ac.kr>
2019-11-15 20:02:37 +0900

    user test cov.100

--- backend/surBing/tests.py
+++ backend/surBing/tests.py
@@ -3,15 +3,6 @@ from django.test import TestCase, Client
 
 # Create your tests here.
 class SurBingTestCase(TestCase):
-    """def test_csrf(self):
-        client = Client(enforce_csrf_checks=True)
-        response = client.post('/api/signup', json.dumps({'username': 'swpp', 'email':'swpp@snu.ac.kr', 'password': 'iluvswpp', 'cart', 'cart'}), content_type='application/json')
-        self.assertEqual(response.status_code, 403)
-
-        response = client.get('/api/token')
-        csrftoken = response.cookies['csrftoken'].value
-        response = client.post('/api/signup', json.dumps({'username': 'swpp', 'email':'swpp@snu.ac.kr', 'password': 'iluvswpp', 'cart', 'cart'}),content_type='application/json', HTTP_X_CSRFTOKEN=csrftoken)
-        self.assertEqual(response.status_code, 201)"""
     def test_token(self):
         # prepare client
         client = Client(enforce_csrf_checks=True)
@@ -24,6 +15,5 @@ class SurBingTestCase(TestCase):
 
     def user_signup(self):
         client = Client()
-        resonse = client.post('/api/signup/', json.dumps({'username':'jomjung', 'password':'1234', 'email':'qwerty@gmail.com'}))
-        self.assertEqual(resonse.status_code, 201)
-
+        response = client.post('/api/signup/', json.dumps({'username':'jomjung', 'password':'1234', 'email':'qwerty@gmail.com'}))
+        self.assertEqual(response.status_code, 201)
