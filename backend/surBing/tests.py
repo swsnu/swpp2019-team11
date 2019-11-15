fe1010b2738e15f63139dec90f496dd9b10d4fc8
geeglegeegle45 <minjung0-0@snu.ac.kr>
2019-11-15 20:37:21 +0900

    test++;

--- backend/surBing/tests.py
+++ backend/surBing/tests.py
@@ -15,10 +15,17 @@ class SurBingTestCase(TestCase):
         response = client.head('/api/token/')
         self.assertEqual(response.status_code, 405) 
 
-    def test_signup(self):
+    def test_user(self):
         client = Client()
+        #signup test
         response = client.post('/api/signup/', json.dumps({'username':'jomjung', 'password':'1234', 'email':'qwerty@gmail.com'}), content_type='application/json')
         self.assertEqual(response.status_code, 201)
+        #signup decode error
+        response = client.post('/api/signup/', json.dumps({'userndame':'jomjung', 'password':'1234', 'email':'qwerty@gmail.com'}), content_type='application/json')
+        self.assertEqual(response.status_code, 400)
+        #signup existing username error
+        response = client.post('/api/signup/', json.dumps({'username':'jomjung', 'password':'1234', 'email':'qwerty@gmail.com'}), content_type='application/json')
+        self.assertEqual(response.status_code, 400)
 
-        response = client.get('/api/checklogin/')
-        self.assertEqual(response.status_code, 200)
+        
+
