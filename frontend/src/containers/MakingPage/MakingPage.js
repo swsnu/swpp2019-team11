a078b68e2dbf7c1cf5d037e2cce856f1eb902f64
geeglegeegle45 <minjung0-0@snu.ac.kr>
2019-11-19 04:53:30 +0900

    makepage

--- frontend/src/containers/MakingPage/MakingPage.js
+++ frontend/src/containers/MakingPage/MakingPage.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import { Sticky, Segment } from 'semantic-ui-react';
 
 export class MakingPage extends Component {
   render() {
     return (
       <div>
-        <h1>This is MakingPage</h1>
+        <Sticky>
+          <Segment>
+            <h1>MakingPage</h1>
+          </Segment>
+        </Sticky>
       </div>
     ); }
 }
