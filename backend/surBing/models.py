9e04337f1710699c21a3a54dd717d28e32259139
geeglegeegle45 <minjung0-0@snu.ac.kr>
2019-11-18 18:13:30 +0900

    fixed models and add comments

--- backend/surBing/models.py
+++ backend/surBing/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 from users.models import SurBingUser
-
+ 
 # Create your models here.
-class Survey(models.Model):
+class Survey(models.Model): #completed survey
     title = models.CharField(max_length=120)
     author = models.ForeignKey(SurBingUser, on_delete=models.CASCADE, related_name='author')
     upload_date = models.CharField(max_length=10, null=True)
@@ -10,6 +10,16 @@ class Survey(models.Model):
     survey_end_date = models.CharField(max_length=10, null=True)
     content = models.TextField(null=True)
     respondant_count = models.IntegerField()
+    item = models.ManyToManyField('Item')
+
+class SurveyOngoing(models.Model):  #not completed survey
+    title = models.CharField(max_length=120)
+    author = models.ForeignKey(SurBingUser, on_delete=models.CASCADE, related_name='author')
+    survey_start_date = models.CharField(max_length=10, null=True)
+    survey_end_date = models.CharField(max_length=10, null=True)
+    content = models.TextField(null=True)
+    respondant_count = models.IntegerField()
+    target_personnel = models.IntegerField()
     item = models.ManyToManyField('Item')
 
 class Item(models.Model):
@@ -22,4 +32,5 @@ class Response(models.Model):
     content = models.TextField()
 
 class Cart(models.Model):
-    survey = models.ManyToManyField('Survey')
+    survey_completed = models.ManyToManyField('Survey')
+    survey_ongoing = models.ManyToManyField('SurveyOngoing')
--- backend/users/models.py
+++ backend/users/models.py
@@ -5,3 +5,8 @@ from django.contrib.auth.models import AbstractUser
 class SurBingUser(AbstractUser):
     cart = models.ForeignKey('surBing.Cart', on_delete=models.PROTECT,
                              related_name='cart', blank=True, null=True)
+    """
+    /*
+    * need to add Survey manytomany field that user participated in. 
+    */
+    """
