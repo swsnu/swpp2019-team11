97c91c734a700f51ee9c3bf101e4102221e2156c
geeglegeegle45 <minjung0-0@snu.ac.kr>
2019-11-18 20:42:57 +0900

    fix urls, add participating_list func

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
+ 
 # Create your models here.
 class SurBingUser(AbstractUser):
     cart = models.ForeignKey('surBing.Cart', on_delete=models.PROTECT,
