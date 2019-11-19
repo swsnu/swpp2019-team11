6e6cd4bcc040e9a648351ce113cf475a26677c43
geeglegeegle45 <minjung0-0@snu.ac.kr>
2019-11-20 03:25:15 +0900

    makingItem error fix

--- frontend/src/components/MakingPage/MakingItem.js
+++ frontend/src/components/MakingPage/MakingItem.js
@@ -1,39 +1,39 @@
 import React, { Component } from 'react';
 import { Segment, Checkbox } from 'semantic-ui-react';
 import { withRouter } from 'react-router-dom';
-import MakingOptions from '../MakingPage/MakingOptions'
+import MakingOptions from './MakingOptions';
 
 export class MakingItem extends Component {
   state = {
     questiontype: 'Subjective',
-    option_count: 1,
   }
-  render() {
+  render () {
     const questionTypeHandler = () => {
-      if (this.state.questiontype == 'Subjective') this.setState({questiontype: 'Selection'})
-      else this.setState({questiontype: 'Subjective'})
+      if (this.state.questiontype == 'Subjective') this.setState({ questiontype: 'Selection' });
+      else this.setState({ questiontype: 'Subjective' });
     };
 
     const onAddhandler = () => {
-      this.props.onAddhandler()
-    }
+      this.props.onAddhandler();
+    };
 
     return (
       <Segment style={{ 'min-height': '250px' }}>
         Q:
         {'  '}
         <input type='text' />
-        <Checkbox toggle onChange={ () => { this.props.onToggle(); questionTypeHandler() } }/>
-        <h3 id="question">{this.props.questiontype}</h3>
+        <Checkbox toggle onChange={() => { this.props.onToggle(); questionTypeHandler(); } }/>
+        <h3 >{this.props.questiontype}</h3>
         {
-          (this.state.questiontype == 'Selection') &&
-          this.props.optionList.map( options => { return (<div><MakingOptions /></div>); })
+          (this.state.questiontype == 'Selection')
+          && this.props.optionList.map(() => { return (<div><MakingOptions /></div>); })
         }
-        {(this.state.questiontype == 'Selection') &&
-          <button onClick={ () => {onAddhandler()} } >Add obtions</button>
+        {
+          (this.state.questiontype == 'Selection')
+          && <button onClick={ () => onAddhandler() }>Add obtions</button>
         }
       </Segment>
     );
   }
-};
+}
 export default withRouter(MakingItem);
