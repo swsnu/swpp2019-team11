5a2b3b35a1cb10b936de63f953911ebee341b7bc
geeglegeegle45 <minjung0-0@snu.ac.kr>
2019-11-20 04:53:50 +0900

    error fix4...

--- frontend/src/containers/MakingPage/MakingPage.js
+++ frontend/src/containers/MakingPage/MakingPage.js
@@ -6,82 +6,82 @@ import MakingItem from '../../components/MakingPage/MakingItem';
 
 export class MakingPage extends Component {
     state= {
-        title: '',
-        content: '',
-        item_list: [
-            { question: '', question_type: 'Subjective', option_list: [ { 'content': '' } ] },
-        ],
+      title: '',
+      content: '',
+      item_list: [
+        { question: '', question_type: 'Subjective', option_list: [ { 'content': '' } ] },
+      ],
     }
 
     render() {
-        const Items = this.state.item_list.map( items => {
-            var onToggleHandler = () => {
-                if (items.question_type == 'Subjective') items.question_type = 'Selection';
-                else items.question_type = 'Subjective';
-                var el = document.getElementById("question");
-                el.innerHTML = items.question_type;
-            }
+      const Items = this.state.item_list.map(items => {
+        var onToggleHandler = () => {
+          if (items.question_type == 'Subjective') items.question_type = 'Selection';
+          else items.question_type = 'Subjective';
+          var el = document.getElementById("question");
+          el.innerHTML = items.question_type;
+        }
 
-            var insertOptionHandler = () => {
-                const new_list = [
-                    ...items.option_list,
-                    { 'content': '' }
-                ];
+        var insertOptionHandler = () => {
+          const new_list = [
+            ...items.option_list,
+            { 'content': '' }
+          ];
 
-                this.setState({
-                    option_list: new_list,
-                });
-                items.option_list = new_list;
-            };
-
-            return (
-                <MakingItem
-                    questiontype={ items.question_type }
-                    optionList={ items.option_list }
-                    onToggle={ () => onToggleHandler() }
-                    onAddhandler={ () => insertOptionHandler() }
-                />
-            );
-        })
-
-        var insertItemHandler = () => {
-            const new_list = [
-                ...this.state.item_list,
-                {
-                    question: '',
-                    question_type: '',
-                }
-            ];
-
-            this.setState({
-                item_list: new_list,
-            });
+          this.setState({
+            option_list: new_list,
+          });
+          items.option_list = new_list;
         };
 
         return (
-            <div>
-                <Sticky>
-                    <Segment><h1>MakingPage</h1></Segment>
-                </Sticky>
-                <Segment>
-                    Title:
-                    {'    '}
-                    <input type="text" onChange={(event) => this.setState({ title: event.target.value })} />
-                    {'    '}
-                    Content:
-                    {'    '}
-                    <input type="text" onChange={(event) => this.setState({ content: event.target.value })} />
-                </Segment>
-                <button onClick={ () => { insertItemHandler() } } >
-                    Add Question Item
-                </button>
-                { Items }
-                <button onClick={ () => { this.props.history.push('/participate/'); } }>
-                    Submit
-                </button>
-            </div>
+          <MakingItem
+            questiontype={ items.question_type }
+            optionList={ items.option_list }
+            onToggle={ () => onToggleHandler() }
+            onAddhandler={ () => insertOptionHandler() }
+          />
         );
-    }
+    })
+
+    var insertItemHandler = () => {
+      const new_list = [
+        ...this.state.item_list,
+        {
+            question: '',
+            question_type: '',
+        }
+      ];
+
+      this.setState({
+        item_list: new_list,
+      });
+    };
+
+    return (
+      <div>
+        <Sticky>
+          <Segment><h1>MakingPage</h1></Segment>
+        </Sticky>
+        <Segment>
+          Title:
+          {'    '}
+          <input type="text" onChange={(event) => this.setState({ title: event.target.value })} />
+          {'    '}
+          Content:
+          {'    '}
+          <input type="text" onChange={(event) => this.setState({ content: event.target.value })} />
+        </Segment>
+        <button onClick={ () => { insertItemHandler() } } >
+          Add Question Item
+        </button>
+        { Items }
+        <button onClick={ () => { this.props.history.push('/participate/'); } }>
+          Submit
+        </button>
+      </div>
+    );
+  }
 }
 
 export default connect(null, null)(withRouter(MakingPage));
