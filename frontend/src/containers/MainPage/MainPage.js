d00f9018f18898c0a66ba316f5dfff443e813cb3
geeglegeegle45 <minjung0-0@snu.ac.kr>
2019-11-25 13:35:22 +0900

    put sidebar of mainpage

--- frontend/src/containers/MainPage/MainPage.js
+++ frontend/src/containers/MainPage/MainPage.js
@@ -4,11 +4,12 @@ import { Grid, Header, Button } from 'semantic-ui-react';
 import { connect } from 'react-redux';
 import SearchBar from '../../components/SearchBar/SearchBar';
 import ProfileButton from '../../components/ProfileButton/ProfileButton';
+import { TopBar } from '../../components/TopBar/TopBar';
 import * as actionCreators from '../../store/actions/index';
 
 export class MainPage extends Component {
   componentDidMount() {
-    this.props.checklogIn().catch(() => { this.props.history.push('/login/'); });
+    //this.props.checklogIn().catch(() => { this.props.history.push('/login/'); });
   }
 
   addSurveyHandler = () => {
@@ -17,24 +18,23 @@ export class MainPage extends Component {
 
   render() {
     return (
-      <Grid className="MainPage" textAlign="center">
-        <Grid.Row textAlign="right" colums={1}>
-          <Grid.Column>
-            <ProfileButton style={{ margin: '25px' }} />
-          </Grid.Column>
-        </Grid.Row>
-        <Grid.Row columns={2} style={{ height: '60.3vh' }} verticalAlign="middle">
-          <Grid.Column>
-            <Header style={{ 'font-size': '4em', cursor: 'pointer' }} size="huge" color="teal" textAlign="center">
-              surBing
-            </Header>
-            <SearchBar size="huge" />
-            <br />
-            <Button className="AddSurveyButton" size="big" color="teal" onClick={() => this.addSurveyHandler()}>Add Your Survey</Button>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-
+      <div>
+        <TopBar />
+        <Grid className="MainPage" textAlign="center">
+          <Grid.Row textAlign="right" colums={1}>
+          </Grid.Row>
+          <Grid.Row columns={2} style={{ height: '60.3vh' }} verticalAlign="middle">
+            <Grid.Column>
+              <Header style={{ 'font-size': '4em', cursor: 'pointer' }} size="huge" color="teal" textAlign="center">
+                surBing
+              </Header>
+              <SearchBar size="huge" />
+              <br />
+              <Button className="AddSurveyButton" size="big" color="teal" onClick={() => this.addSurveyHandler()}>Add Your Survey</Button>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </div>
     );
   }
 }
