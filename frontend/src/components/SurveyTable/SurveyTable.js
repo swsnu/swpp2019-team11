a626eccdddff1d7639ac7bf5def6c1ffb18b8a87
Kojihyung <kokoleerh@gmail.com>
2019-11-29 02:30:15 +0900

    table

---
+++ frontend/src/components/SurveyTable/SurveyTable.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {
+   Button, Icon, Table, Checkbox
+} from 'semantic-ui-react';
+import { withRouter } from 'react-router-dom';
+
+export const SurveyTable = (props) => {
+    return(
+            <Table.Row>
+              <Table.Cell collapsing>
+                <Checkbox slider />
+              </Table.Cell>
+              <Table.Cell>{props.survey.title}</Table.Cell>
+              <Table.Cell>{props.survey.author}</Table.Cell>
+              <Table.Cell>{props.survey.respondant_count}</Table.Cell>
+              <Table.Cell>{props.survey.content}</Table.Cell>
+            </Table.Row>
+
+    )
+}
+  
+  
+export default withRouter(SurveyTable);
+  
