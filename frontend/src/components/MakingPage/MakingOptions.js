9b2a6d4e181ce90bae9d60c40e5f94ee28ef45aa
geeglegeegle45 <minjung0-0@snu.ac.kr>
2019-12-03 18:36:58 +0900

    eslint fixed

--- frontend/src/components/MakingPage/MakingOptions.js
+++ frontend/src/components/MakingPage/MakingOptions.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Input } from 'semantic-ui-react';
 import { withRouter } from 'react-router-dom';
-import './MakingOptions.css'
+import './MakingOptions.css';
 
 export const MakingOptions = (props) => (
   <div className="MakingOptions">
-    <div id="optionNumber">{props.number}: </div>
+    <div id="optionNumber">
+      {props.number}
+:
+      {' '}
+    </div>
     <Input className="OptionInput" placeholder="Write Options..." onChange={(e) => props.content(e.target.value, props.number)} />
     {' '}
     <br />
