b47ddfeea83f7799fd75ee58ec474c4f805ae3ab
geeglegeegle45 <minjung0-0@snu.ac.kr>
2019-12-07 22:38:50 +0900

    eslint fix

--- frontend/src/components/TableForm/TableForm.test.js
+++ frontend/src/components/TableForm/TableForm.test.js
@@ -3,15 +3,17 @@ import { shallow } from 'enzyme';
 import { TableForm } from './TableForm';
 
 describe('TableForm', () => {
-    const props = {
-        slide: true,
-        content: [
-            { title: 'mockTitle', author: 'mockAuthor', respondant_count: 1, content: 'mockContent' }
-        ]
-    };
-    it('test1', () => {
-        const component = shallow(<TableForm {...props} />);
-        const wrapper = component.find('#Table');
-        expect(wrapper.length).toBe(1);
-    });
+  const props = {
+    slide: true,
+    content: [
+      {
+        title: 'mockTitle', author: 'mockAuthor', respondant_count: 1, content: 'mockContent',
+      },
+    ],
+  };
+  it('test1', () => {
+    const component = shallow(<TableForm {...props} />);
+    const wrapper = component.find('#Table');
+    expect(wrapper.length).toBe(1);
+  });
 });
