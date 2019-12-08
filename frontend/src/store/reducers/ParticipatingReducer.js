6b11017a4d6ae6976326c8129a5a5db1ed960cc3
jangsus1 <min99101511@naver.com>
2019-12-09 01:03:19 +0900

    participated list in reducer implementing

--- frontend/src/store/reducers/ParticipatingReducer.js
+++ frontend/src/store/reducers/ParticipatingReducer.js
@@ -2,12 +2,15 @@ import * as actionTypes from '../actions/actionTypes';
 
 const initialState = {
   survey_list: [],
+  participated_list : []
 };
 
 const ParticipatingReducer = (state = initialState, action = actionTypes.DEFAULT) => {
   switch (action.type) {
     case actionTypes.GET_PARTICIPATING_LIST:
       return { ...state, survey_list: action.target };
+    case actionTypes.GET_PARTICIPATED_LIST:
+      return { ...state, participated_list: action.target}
     default:
       break;
   }
